228573195cc033423acecac14f4ca1a9ff2e7d78
wzyb-52 <1534511935@qq.com>
2022-08-15 23:27:25 +0800

    perfect hashCode Complished

--- hw3/hw3/hash/SimpleOomage.java
+++ hw3/hw3/hash/SimpleOomage.java
@@ -26,7 +26,7 @@ public class SimpleOomage implements Oomage {
         if (!USE_PERFECT_HASH) {
             return red + green + blue;
         } else {
-            return (red / 5) * 53 * 53 + (green / 5) * 47 + blue / 5 ;
+            return (red / 5) * 53 * 53 + (green / 5) * 53 + blue / 5 ;
         }
     }
 
