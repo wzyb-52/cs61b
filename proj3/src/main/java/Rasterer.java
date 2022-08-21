a46e22d9ae6d5c4aa575d660e8c1fbabf8428991
wzyb-52 <1534511935@qq.com>
2022-08-22 02:54:45 +0800

    Part 1 Rasterer finished

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -9,8 +9,32 @@ import java.util.Map;
  */
 public class Rasterer {
 
+    /** Add some private static variables to make life easier. */
+    private static double ROOT_ULLAT, ROOT_ULLON, ROOT_LRLAT, ROOT_LRLON;
+    private static int TILE_SIZE;
+    private static double[] LonDPPSofAllLevels, LatDPPSofAllLevels;
+
     public Rasterer() {
         // YOUR CODE HERE
+        /** Just copy these variables from MapServer. */
+        ROOT_ULLAT = MapServer.ROOT_ULLAT;
+        ROOT_ULLON = MapServer.ROOT_ULLON;
+        ROOT_LRLAT = MapServer.ROOT_LRLAT;
+        ROOT_LRLON = MapServer.ROOT_LRLON;
+        TILE_SIZE  = MapServer.TILE_SIZE;
+        getLonAndLatDPPs();
+    }
+
+    /** Caculates LonDPPs and LatDPPs of all 8 levels of graph sets. */
+    private void getLonAndLatDPPs() {
+        LonDPPSofAllLevels = new double[8];
+        LatDPPSofAllLevels = new double[8];
+        LonDPPSofAllLevels[0] = (ROOT_LRLON - ROOT_ULLON) / TILE_SIZE;
+        LatDPPSofAllLevels[0] = (ROOT_ULLAT - ROOT_LRLAT) / TILE_SIZE;
+        for (int i = 1; i < 8; ++i) {
+            LonDPPSofAllLevels[i] = LonDPPSofAllLevels[i - 1] / 2;
+            LatDPPSofAllLevels[i] = LatDPPSofAllLevels[i - 1] / 2;
+        }
     }
 
     /**
@@ -43,9 +67,66 @@ public class Rasterer {
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
         // System.out.println(params);
+
+        /** Finds the depth according to the appropriate LonDPP,
+         *  then calculates the LonDPP and LatDPP of raster. */
+        double lrlon = params.get("lrlon"), ullon = params.get("ullon"),
+            ullat = params.get("ullat"), lrlat = params.get("lrlat");
+        double queryLonDPP = (lrlon - ullon) / params.get("w");
+        int depth;
+        for (depth = 0; depth < 8; ++depth) {
+            if (LonDPPSofAllLevels[depth] <= queryLonDPP) {
+                break;
+            }
+        }
+        if (depth == 8) {
+            depth = 7;  // depth is at most 7.
+        }
+        double rasterLonDPP = LonDPPSofAllLevels[depth], rasterLatDPP = LatDPPSofAllLevels[depth];
+
+        /** Finds the indexes of four tiles in corners */
+        int xul, xlr, yul, ylr, MAXINDEX = (int) Math.pow(2, depth) - 1;
+        xul = ullon <= ROOT_ULLON ? 0
+            : (ullon > ROOT_LRLON ? MAXINDEX : (int) ((ullon - ROOT_ULLON) / rasterLonDPP / TILE_SIZE));
+        xlr = lrlon <= ROOT_ULLON ? 0
+            : (lrlon > ROOT_LRLON ? MAXINDEX : (int) ((lrlon - ROOT_ULLON) / rasterLonDPP / TILE_SIZE));
+        yul = ullat >= ROOT_ULLAT ? 0
+            : (ullat < ROOT_LRLAT ? MAXINDEX : (int) ((ROOT_ULLAT - ullat) / rasterLatDPP / TILE_SIZE));
+        ylr = lrlat >= ROOT_ULLAT ? 0
+            : (lrlat < ROOT_LRLAT ? MAXINDEX : (int) ((ROOT_ULLAT - lrlat) / rasterLatDPP / TILE_SIZE));
+
+        /** Calculates the coordinates of four corners of raster */
+        double raster_ul_lon, raster_lr_lon, raster_ul_lat, raster_lr_lat;
+        raster_ul_lon = ROOT_ULLON + xul * TILE_SIZE * rasterLonDPP;
+        raster_lr_lon = ROOT_ULLON + (xlr + 1) * TILE_SIZE * rasterLonDPP;
+        raster_ul_lat = ROOT_ULLAT - yul * TILE_SIZE * rasterLatDPP;
+        raster_lr_lat = ROOT_ULLAT - (ylr + 1) * TILE_SIZE * rasterLatDPP;
+
+        /** Concatenates name strings of all tiles */
+        String[][] render_grid = new String[ylr - yul + 1][xlr - xul + 1];
+        for (int y = 0; y < ylr - yul + 1; ++y) {
+            for (int x = 0; x < xlr - xul + 1; ++x) {
+                render_grid[y][x] = String.format("d%d_x%d_y%d.png", depth, x + xul, y + yul);
+            }
+        }
+
+        /** I'm not sure for the criteria of the query success */
+        boolean query_success = true;
+        if (ullon >= ROOT_LRLON || lrlon <= ROOT_ULLON || ullat <= ROOT_LRLAT || lrlat >= ROOT_ULLAT) {
+            query_success = false;
+        }
+
+        /** Finally constructs the return map with all information of our raster */
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+//        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
+//                           + "your browser.");
+        results.put("depth", depth);
+        results.put("query_success", query_success);
+        results.put("raster_ul_lon", raster_ul_lon);
+        results.put("raster_lr_lon", raster_lr_lon);
+        results.put("raster_lr_lat", raster_lr_lat);
+        results.put("raster_ul_lat", raster_ul_lat);
+        results.put("render_grid", render_grid);
         return results;
     }
 
